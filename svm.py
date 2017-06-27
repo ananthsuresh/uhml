409a6601fae27c9263c5d3aa039703f4b231ebf2
Ananth <ananth.pro@gmail.com>
2017-06-27 14:38:56 -0400

    added some scoring

--- svm.py
+++ svm.py
@@ -5,7 +5,7 @@ from matplotlib import style
 style.use("ggplot")
 from sklearn import svm
 from sklearn.model_selection import KFold, cross_val_score
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix, f1_score
 
 #loading breast cancer data from UCI ML Repo
 url = "https://goo.gl/AP7kzV"
@@ -22,18 +22,30 @@ k_fold = KFold(n_splits=5)
 #for train_indices, test_indices in k_fold.split(X):
     #print('Train: %s | test: %s' % (train_indices, test_indices))
 
-svc = svm.SVC(C=1, kernel='linear')
+svc = svm.SVC(C=0.1, kernel='linear')
 
 #to change all nan values to 0
 X[(np.isnan(X))] = 0
 
 #loop to do 5 fold cross validation, stores scores in array
-scores = [svc.fit(X[train], y[train]).score(X[test], y[test])
-    for train, test in k_fold.split(X)]
-print(scores)
+# scores = [svc.fit(X[train], y[train]).score(X[test], y[test])
+#     for train, test in k_fold.split(X)]
+# print(scores)
+i = 1
 
-# for train, test in k_fold.split(X):
-#     svc.fit(X[train], y[train])
-#     predicted = svc.predict(X[test])
-#     report = classification_report(y[test], predicted)
-#     print(report)
+for train, test in k_fold.split(X):
+    fitted = svc.fit(X[train], y[train])
+    accuracy = fited.score(X[test], y[test])
+    predicted = svc.predict(X[test])
+    report = classification_report(y[test], predicted)
+    confusion = confusion_matrix(y[test], predicted)
+    f1 = f1_score(y[test], predicted)
+    print("Scores for Fold %d \n", i)
+    print(report)
+    print("\n")
+    print("Accuracy Score: %f \n",accuracy)
+    print("Confusion Matrix: \n")
+    print(confusion)
+    print("\n")
+    print("F1 score: %f \n", f1)
+    i++
