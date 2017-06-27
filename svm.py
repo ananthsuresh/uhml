7baef120854efb78c5b96edb719665c6b14abf33
Ananth <ananth.pro@gmail.com>
2017-06-27 10:52:08 -0400

    s

--- svm.py
+++ svm.py
@@ -21,7 +21,7 @@ k_fold = KFold(n_splits=5)
 #for train_indices, test_indices in k_fold.split(X):
     #print('Train: %s | test: %s' % (train_indices, test_indices))
 
-svc = svm.SVC(C=1, kernel='linear')
+svc = svm.SVC(C=500, kernel='linear')
 
 #to change all nan values to 0
 X[(np.isnan(X))] = 0
@@ -29,5 +29,10 @@ X[(np.isnan(X))] = 0
 #loop to do 5 fold cross validation, stores scores in array
 scores = [svc.fit(X[train], y[train]).score(X[test], y[test])
     for train, test in k_fold.split(X)]
-
 print(scores)
+
+for train, test in k_fold.split(X):
+    svc.fit(X[train], y[train])
+    predicted = model.predict(X[test])
+    report = classification_report(y[test], predicted)
+    print(report)
