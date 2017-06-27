8e04980ccad2e5495beb295c4dfaf3f6d9bc0487
Ananth <ananth.pro@gmail.com>
2017-06-27 10:52:39 -0400

    s

--- svm.py
+++ svm.py
@@ -5,6 +5,7 @@ from matplotlib import style
 style.use("ggplot")
 from sklearn import svm
 from sklearn.model_selection import KFold, cross_val_score
+from sklearn.metrics import classification_report
 
 #loading breast cancer data from UCI ML Repo
 url = "https://goo.gl/AP7kzV"
